24d197e1b08b1464f38d30c5aa736f0902fff96c
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-16 14:04:37 +0100

    set up landing-page

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,8 @@
 import { Routes } from '@angular/router';
+import { LandingPageComponent } from './pages/landing-page/landing-page.component';
+import { PortfolioPageComponent } from './pages/portfolio-page/portfolio-page.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {path:'', title:'Home', component:LandingPageComponent},
+    {path:'portfolio', title:'portfolio', component:PortfolioPageComponent},
+];
