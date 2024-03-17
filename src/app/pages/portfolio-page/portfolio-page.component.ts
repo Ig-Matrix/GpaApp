e1718c4e451eea7617cb052ee7f9037fde9f5a4d
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-17 11:59:00 +0100

    progress bar integration

--- src/app/pages/portfolio-page/portfolio-page.component.ts
+++ src/app/pages/portfolio-page/portfolio-page.component.ts
@@ -3,17 +3,20 @@ import { NavComponent } from '../../components/portfolio/nav/nav.component';
 import { AddCourseModalComponent } from '../../components/add-course-modal/add-course-modal.component';
 import { Course } from '../../models/interfaces/Course';
 import { NgClass } from '@angular/common';
+import { ProgressComponent } from '../../components/progress/progress.component';
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [NavComponent, AddCourseModalComponent, NgClass],
+  imports: [NavComponent, AddCourseModalComponent, NgClass, ProgressComponent],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.css',
 })
 export class PortfolioPageComponent {
   courses: Course[] = [];
   isShowModal: boolean = false;
+  cgpa!: number;
+  isGpaCalculated: boolean=false
 
   openModal() {
     this.isShowModal = true;
@@ -27,6 +30,8 @@ export class PortfolioPageComponent {
     course.grade = this.calculateGrade(course.score);
     this.courses.push(course);
     this.isShowModal = false;
+    this.cgpa = 0;
+    this.isGpaCalculated=false;
   }
 
   calculateGrade(score: number): string {
@@ -45,10 +50,49 @@ export class PortfolioPageComponent {
     }
   }
 
+  calculateGpa(): number {
+    this.isGpaCalculated=true;
+    let totalCreditUnits = 0;
+    let totalQualityPoints = 0;
+
+    for (const course of this.courses) {
+      const gradeUnit = this.getGradePoints(course.score);
+      totalCreditUnits += course.unit;
+      totalQualityPoints += gradeUnit * course.unit;
+    }
+
+    if (totalCreditUnits <= 0) {
+      this.cgpa = 0;
+    } else {
+      this.cgpa = totalQualityPoints / totalCreditUnits;
+    }
+    return this.cgpa;
+  }
+
   deleteCourse(course: Course) {
     const deleteIndex = this.courses.findIndex((c) => c === course);
     if (deleteIndex !== -1) {
       this.courses.splice(deleteIndex, 1);
+      this.cgpa = 0;
+      this.isGpaCalculated=false
+    }
+  }
+
+  getGradePoints(score: number): number {
+    if (score >= 70) {
+      return 5.0;
+    } else if (score >= 60) {
+      return 4.0;
+    } else if (score >= 50) {
+      return 3.0;
+    } else if (score >= 45) {
+      return 2.0;
+    } else if (score >= 40) {
+      return 1.0;
+    } else if (score >= 1) {
+      return (score / 39) * 0.99;
+    } else {
+      return 0; // Return null for scores below 1
     }
   }
 }
