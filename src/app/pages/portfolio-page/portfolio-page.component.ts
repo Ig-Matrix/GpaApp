a70aac3b01f9abed24402619c83110a6dcf7c136
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-17 08:10:22 +0100

    render courses dynamically in the table

--- src/app/pages/portfolio-page/portfolio-page.component.ts
+++ src/app/pages/portfolio-page/portfolio-page.component.ts
@@ -2,11 +2,12 @@ import { Component } from '@angular/core';
 import { NavComponent } from '../../components/portfolio/nav/nav.component';
 import { AddCourseModalComponent } from '../../components/add-course-modal/add-course-modal.component';
 import { Course } from '../../models/interfaces/Course';
+import { NgClass } from '@angular/common';
 
 @Component({
   selector: 'app-portfolio-page',
   standalone: true,
-  imports: [NavComponent, AddCourseModalComponent],
+  imports: [NavComponent, AddCourseModalComponent, NgClass],
   templateUrl: './portfolio-page.component.html',
   styleUrl: './portfolio-page.component.css',
 })
@@ -23,7 +24,24 @@ export class PortfolioPageComponent {
   }
 
   addCourse(course: Course){
+    course.grade= this.calculateGrade(course.score)
     this.courses.push(course)
     this.isShowModal=false
+  }
+
+  calculateGrade(score: number): string{
+    if (score >= 70) {
+      return 'A';
+    } else if (score >= 60) {
+      return 'B';
+    } else if (score >= 50) {
+      return 'C';
+    } else if (score >= 45) {
+      return 'D';
+    } else if (score >= 40) {
+      return 'E';
+    } else {
+      return 'F';
+    }
   }
 }
