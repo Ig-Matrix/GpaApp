93e14adb4bc41c2fa3698c84ac82f8e27e4cc1ae
Ig-matrix <omomaighawosa440@gmail.com>
2024-03-17 16:14:07 +0100

    implement modal algorithm

--- src/app/components/portfolio/edit-course-modal/edit-course-modal.component.ts
+++ src/app/components/portfolio/edit-course-modal/edit-course-modal.component.ts
@@ -1,4 +1,10 @@
-import { Component, EventEmitter, HostListener, Input, Output } from '@angular/core';
+import {
+  Component,
+  EventEmitter,
+  HostListener,
+  Input,
+  Output,
+} from '@angular/core';
 import { FormsModule } from '@angular/forms';
 import { Course } from '../../../models/interfaces/Course';
 import { NgClass } from '@angular/common';
@@ -8,23 +14,21 @@ import { NgClass } from '@angular/common';
   standalone: true,
   imports: [FormsModule, NgClass],
   templateUrl: './edit-course-modal.component.html',
-  styleUrl: './edit-course-modal.component.css'
+  styleUrl: './edit-course-modal.component.css',
 })
 export class EditCourseModalComponent {
   @Input() showModal!: boolean;
+  @Input() course!: Course;
   @Output() closeModalEvent = new EventEmitter();
   @Output() coursesEditted = new EventEmitter<Course>();
-
-  course: Course = { name: '', code: '', score: 0, unit: 0 };
 
   closeModal(event: Event): void {
     event.stopPropagation();
     this.showModal = false;
-    this.course = { name: '', code: '', score: 0, unit: 0 };
     this.closeModalEvent.emit();
   }
 
-  submitCourse(event: Event) {
+  saveCourse(event: Event) {
     this.coursesEditted.emit(this.course);
     this.closeModal(event);
   }
